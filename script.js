d3ac78c483bc8f7b995c99f7bb1f33f47384339b
jdegand <70610011+jdegand@users.noreply.github.com>
2021-03-03 13:24:10 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('share').addEventListener('click', ()=> {
+  document.getElementById('tooltip-content').classList.toggle('active')
+})
+
+document.getElementById('mobile-share').addEventListener('click', ()=> {
+  document.getElementById('tooltip-content').classList.toggle('active')
+})
